50d64d2a7338dfff4d447ef74f0cdef00ce11eef
Mike Eberhart <42505829+MikeEberhart@users.noreply.github.com>
2025-09-29 08:38:42 -0400

    Working on Main Menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    public GameObject shields;
     public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        shields.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            //something like this but needs a bool to check if the player has the power up
+            shields.SetActive(true);
+        }
     }
 
     private void PlayerMovement()
@@ -33,4 +41,5 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over");
         }
     }
+
 }
